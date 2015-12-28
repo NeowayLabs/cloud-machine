67f4d9749a5ddcb2a6cc770c87e4eb3b7b72f51f
Guilherme Santos <guilherme.santos@neoway.com.br>
2015-12-28 10:59:48 -0200

    Avoid try to create volume if AvailableZone is empty
    - Thanks @katcipis

--- volume/volume.go
+++ volume/volume.go
@@ -132,6 +132,10 @@ func Load(ec2Ref *ec2.EC2, volume *Volume) (ec2.Volume, error) {
 
 // Create new volume
 func Create(ec2Ref *ec2.EC2, volume *Volume) (ec2.Volume, error) {
+	if volume.AvailableZone == "" {
+		return ec2.Volume{}, errors.New("volume.AvailableZone is a required parameter")
+	}
+
 	options := ec2.CreateVolume{
 		VolumeType: volume.Type,
 		AvailZone:  volume.AvailableZone,
